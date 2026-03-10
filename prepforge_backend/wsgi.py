d6f954e48692820e77b66d0ea0a87e9083006864
Shivaani Nainwal <jia18feb@gmail.com>
2026-03-10 16:00:00 +0530

    Update DJANGO_SETTINGS_MODULE to use backend settings

--- prepforge_backend/wsgi.py
+++ prepforge_backend/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prepforge_backend.settings')
 
 application = get_wsgi_application()
